b206b8d8f1eff66f8086f469547cdebc711ae378
sago-miho <s.miho.1168@gmail.com>
2025-09-08 01:17:20 +0900

    2-6

---
+++ 2-6/Task2_6.java
@@ -0,0 +1,32 @@
+public class Task2_6 {
+
+  public static void main(String[] args) {
+
+    // ①int型の hogeInt という名前の変数を宣言し、値「50」で初期化
+        int hogeInt = 50;
+
+        // ④変数 hogeInt を値「100」で上書き
+        hogeInt = 100;
+
+        // ⑥変数 hogeInt の値を表示
+        System.out.println(hogeInt);
+
+    // ②Stringクラスの hogeString という名前の変数を宣言、値「テスト」で初期化
+        String hogeString = "テスト";
+
+        // ⑤変数 hogeString を値「良くできました！」で上書き
+        hogeString = "良くできました！";
+    
+    // ③下記の処理について、何をしているのかコメントを記入してください
+        // double型の hogeDouble という名前の変数を宣言し、値「0.8」で初期化
+        double hogeDouble = 0.8;
+
+        // ⑧変数 hogeDouble の値を表示
+        System.out.println(hogeDouble);
+
+    // ⑦下記の処理について、何をしているのかコメントを記入してください
+        // 変数 hogeString の値を表示
+        System.out.println(hogeString);
+
+  }
+}
